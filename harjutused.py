df3162a5a207860990a1421a734adb6b05dbccc4
Rayze64 <raymond.parts64@gmail.com>
2021-01-26 13:38:55 +0200

    tehtud yes very nice

--- harjutused.py
+++ harjutused.py
@@ -1,8 +1,15 @@
+import math
 # Harjutus 1 - Leia vahemik
 # Looge funktsioon, mis võtab sisendiks ühe arvu
 # Funktsioon peab kontrollima, kas antud arv kuulub vahemikku 0 kuni 100 või 101 kuni 1000
     # Kui kuulub vahemikku 0 kuni 100, siis tuleb printida tekst "Arv on vahemikus 0st 100ni".
     # Kui kuulub vahemikku 101 kuni 1000, siis tuleb printida tekst "Arv on vahemikus 101st 1000ni".
+
+arv = 952
+if 0 < arv <= 101:
+    print("Arv on vahemikus 0st 100ni")
+elif 101 < arv <= 1000:
+    print("Arv on vahemikus 101st 1000ni")
 
 
 # Harjutus 2 - prindi negatiivsed arvud
@@ -12,12 +19,19 @@ arvud = [5, 9, 1, -2, 6, -15, -20]
 # Looge funktsioon, mis käib tsükliga kõik arvud listis läbi ning iga arvu puhul kontrollib, kas arv on negatiivne.
     # Kui arv on negatiivne, siis printige see välja
 
+for num in arvud:
+    if num < 0:
+        print(num)
 
 # Harjutus 3 - boonus
 # Looge funktsioon, mis võtab sisendiks listi, mis koosneb 4st täis arvust.
 # Funktsioon peab iga listi elemendi puhul printima uuele reale nii mitu $ sümbolit kui elemendi väärtus ütleb
 
 # Näiteks listi sisend = [1, 3, 4, 3] puhul prinditakse järgnev tulemus
+sisend = [1, 3, 4, 3]
+for num in sisend:
+    print("$" * num)
+
 #$
 #$$$
 #$$$$
