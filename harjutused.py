a9337a57a3a37c44de66e7177edc7eec2da28bd8
Rayze64 <raymond.parts64@gmail.com>
2021-01-26 13:58:20 +0200

    harjutused ok

--- harjutused.py
+++ harjutused.py
@@ -16,7 +16,6 @@ elif 101 < arv <= 1000:
 # Ette on antud arvude list
 arvud = [5, 9, 1, -2, 6, -15, -20]
 
-
 # Looge funktsioon, mis käib tsükliga kõik arvud listis läbi ning iga arvu puhul kontrollib, kas arv on negatiivne.
     # Kui arv on negatiivne, siis printige see välja
 
